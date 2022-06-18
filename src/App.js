45951f3316806e769a4eab58c50707fc2249f575
Ashwin1729 <mathematicswroteus@gmail.com>
2022-06-19 05:22:31 +0530

    Days grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { getMonth } from "./util";
 
-function App() {
+const App = () => {
+  console.table(getMonth(4));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div>Lets Do It</div>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
