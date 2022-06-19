bd0d9f1ca35993ae0f9e4f70637a95afee786f36
Ashwin1729 <mathematicswroteus@gmail.com>
2022-06-19 14:41:59 +0530

    calendar grid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { getMonth } from "./util";
+import CalendarHeader from "./components/ClaendarHeader";
+import Sidebar from "./components/Sidebar";
+import Month from "./components/Month";
 
 const App = () => {
-  console.table(getMonth(4));
+  const [currentMonth, setCurrentMonth] = useState(getMonth());
+  console.table(getMonth());
   return (
     <React.Fragment>
-      <div>Lets Do It</div>
+      <div className="h-screen flex felx-columns">
+        <CalendarHeader />
+        <div className="flex flex-1">
+          <Sidebar />
+          <Month month={currentMonth} />
+        </div>
+      </div>
     </React.Fragment>
   );
 };
